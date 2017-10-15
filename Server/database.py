1b0a3fc49461472b68983cb494bab1d5b186eae0
Sharad Bhat <sharad.mbhat@gmail.com>
2017-10-16 01:34:53 +0530

    Update database.py

--- Server/database.py
+++ Server/database.py
@@ -7,13 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class Database:
     def __init__(self): # WORKS
-<<<<<<< HEAD:Server/database.py
         self.db = pymysql.connect(host="localhost", user="root", passwd="*********", db="video")
-=======
-        self.db = pymysql.connect(host="localhost", user="root", passwd="", db="video")
->>>>>>> d7794a60207114c2ecad21b2604e729588214f3a:database.py
-        self.cur = self.db.cursor()
-
     def get_most_viewed(self):
         """
         - Returns a list of top 10 video IDs in the descending order of view count from the VIDEOS table.
@@ -164,7 +158,6 @@ class Database:
         - Returns a random video ID from the VIDEOS table.
         """
         self.cur.execute("SELECT video_ID FROM videos ORDER BY RAND() LIMIT 1") # Selects video_ID from 1 random row.
-<<<<<<< HEAD:Server/database.py
         return self.cur.fetchone()[0]
 
     def get_watched(self, username):
@@ -195,19 +188,3 @@ class Database:
             os.remove('static/images/' + str(video_ID) + '.jpg')
         except:
             self.db.rollback()
-=======
-	return self.cur.fetchone()[0]
-
-#Admin part
-    def add_admin(self, username, password):
-	"""
-	- Add the new admin credentials to the ADMINS table.
-	"""
-        password_hash = generate_password_hash(password) # Generates a SHA256 hash.
-        try:
-            self.cur.execute("INSERT INTO ADMINS VALUES(\"{}\", \"{}\")".format(username, password_hash))
-            self.db.commit()
-        except:
-            self.db.rollback()
-
->>>>>>> d7794a60207114c2ecad21b2604e729588214f3a:database.py
